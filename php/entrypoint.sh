673237d83624482704147f5c4c082b655e457d0c
Kirill <mr.ikirill@me.com>
2018-06-04 11:40:01 +1000

    Update entrypoint.sh
    update init project process

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -15,7 +15,7 @@ function destinationExists(){
   fi
 }
 
-#get file size of remote file in MB
+#get a file size of remote file in MB
 function getFileSize() {
   FILE_SIZE=`wget -S --spider $1 2>&1 | awk '/Content-Length/ { print $2 }'`;
   if [[ ${FILE_SIZE} > 0 ]];
@@ -24,15 +24,18 @@ function getFileSize() {
   fi
 }
 
+#clean temp dir
 rm -rf ${TEMP_DIR}/*
 
 # Bitrix not installed
 if [ ! -d "${APPLICATION_DIR}/bitrix" ]; then
+  #clean application dir
+  rm -rf ${APPLICATION_DIR}/*
 
   EXISTS=true
   COUNTER=0
 
-# Download backup(s)
+# Download
   while [ ${EXISTS} = true ]
        do
            URL=${BACKUP_URL}
@@ -44,50 +47,49 @@ if [ ! -d "${APPLICATION_DIR}/bitrix" ]; then
            FILE_SIZE=$(getFileSize "${URL}");
 
            if (( ${FILE_SIZE} < 10 ));
-             then EXISTS=false;
+              then EXISTS=false;
            fi
 
-           if [ ${EXISTS} = true ]; 
-             then wget ${URL} -P ${TEMP_DIR}
+           if [ ${EXISTS} = true ];
+              then wget ${URL} -P ${TEMP_DIR}
            fi
 
            let COUNTER=COUNTER+1
   done
-
         # Full archive name without path
         ARCHIVE="${BACKUP_URL##*/}"
         # Archive name without .tar.gz
         ARCHIVE_NAME="${ARCHIVE::-7}"
 
+        cat ${TEMP_DIR}/${ARCHIVE}* > ${TEMP_DIR}/archive.tar.gz
+        rm -rf ${TEMP_DIR}/${ARCHIVE}*
+        # Extract
+        tar -xzf ${TEMP_DIR}/archive.tar.gz -C ${TEMP_DIR}
+        # Delete archive
+        rm ${TEMP_DIR}/archive.tar.gz
         # Restore project from archive
-        if [ ${INIT} = true ]; then
-         cp -r ${TEMP_DIR}/* ${APPLICATION_DIR}/
-         wget ${BITRIX_SETUP} -O ${APPLICATION_DIR}/restore.php
+        if [ ${INIT} = true ];
+            then
+              cp -R ${TEMP_DIR}/* ${APPLICATION_DIR}
+              wget ${BITRIX_SETUP} -O ${APPLICATION_DIR}/restore.php
         else
-              cat ${TEMP_DIR}/${ARCHIVE}* > ${TEMP_DIR}/archive.tar.gz
-              rm -rf ${TEMP_DIR}/${ARCHIVE}*
-              # Extract
-              tar -xzf ${TEMP_DIR}/archive.tar.gz -C ${TEMP_DIR}
+            # Copy database file to root folder
+            if [ -f "${TEMP_DIR}/bitrix/backup/${ARCHIVE_NAME}.sql" ];
+                then mv ${TEMP_DIR}/bitrix/backup/${ARCHIVE_NAME}.sql ${TEMP_DIR}/bitrix.sql
+            fi
 
-              # Delete archive
-              rm ${TEMP_DIR}/archive.tar.gz
-
-              # Copy database file to root folder
-             if [ -f "${TEMP_DIR}/bitrix/backup/${ARCHIVE_NAME}.sql" ]; then
-                 mv ${TEMP_DIR}/bitrix/backup/${ARCHIVE_NAME}.sql ${TEMP_DIR}/bitrix.sql
-             fi
-
-             if [ -f "${TEMP_DIR}/bitrix/backup/${ARCHIVE_NAME}_after_connect.sql" ]; then
-                 mv ${TEMP_DIR}/bitrix/backup/${ARCHIVE_NAME}_after_connect.sql ${TEMP_DIR}/bitrix_after_connect.sql
-             fi
+            if [ -f "${TEMP_DIR}/bitrix/backup/${ARCHIVE_NAME}_after_connect.sql" ];
+                then mv ${TEMP_DIR}/bitrix/backup/${ARCHIVE_NAME}_after_connect.sql ${TEMP_DIR}/bitrix_after_connect.sql
+            fi
 
             rm -rf ${APPLICATION_DIR}/
             git clone ${REPO_URL} ${APPLICATION_DIR}
-            cp -r "${TEMP_DIR}/bitrix" ${APPLICATION_DIR}
+            cp -R ${TEMP_DIR}/bitrix ${APPLICATION_DIR}
             cp ${TEMP_DIR}/bitrix.sql ${APPLICATION_DIR}/bitrix.sql
             cp ${TEMP_DIR}/bitrix_after_connect.sql ${APPLICATION_DIR}/bitrix_after_connect.sql
-
         fi
+        #clean temp dir
+        rm -rf ${TEMP_DIR}/*
 fi
 
 # Run PHP-FPM
